6ad6aa9ce9114184caf86f11a0868f7ff67c0af7
matthew-dean-brown <mattrewbrown@gmail.com>
2024-02-11 23:46:51 +0200

    update

--- routes/login.js
+++ routes/login.js
@@ -1,8 +1,13 @@
 import express from 'express';
-
+import jwt from 'jsonwebtoken';
 const router = express.Router()
 
 router.post('/', (req,res)=>{
+    // console.log(req.body.email);
+    const user = {email:req.body.email};
+     
+    const token = jwt.sign({user}, 'my_secret_key',{expiresIn: '1hr'});
+    res.cookie('jwt',token,{httpOnly:true});
     res.send({
         message:`Hello ${req.body.email}, You have logged in, Yay!`
     })
--- server.js
+++ server.js
@@ -1,16 +1,20 @@
 import express from "express";
 import cors from "cors";
-import loginRouter from './routes/login.js'
 import infoRouter from './routes/info.js'
+import loginRouter from './routes/login.js'
 
+const ensureToken = (req,res,next) =>{
+    console.log(req.body)
+    next()
+}
 const app = express()
 
 app.use(cors())
 app.use(express.json())
-app.use(express.static('./views'))
-
-app.use('/api', infoRouter)
+app.use(express.static('/views'))
+app.use('/api',ensureToken,infoRouter)
 app.use('/register', loginRouter)
+
 
 
 
